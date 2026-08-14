f10b99123ac2cca38f2a78c09606bf12ba8274ab
Berat Bedirhan Barbaros <barbarosb966@gmail.com>
2026-08-15 02:45:58 +0300

    Add service worker for NightQueen stopwatch

---
+++ projects/nightqueen-stopwatch/sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'nightqueen-stopwatch-v1';
+const APP_SHELL = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
